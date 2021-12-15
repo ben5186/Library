305a7feba96efdd1181eb5d91e92e64a455f8a53
Ben <50188950+ShavedWhale1@users.noreply.github.com>
2021-12-15 12:16:59 -0500

    Update Library.py

--- Library.py
+++ Library.py
@@ -111,6 +111,8 @@ def returnBook(book):
                             fineList.append([book[secondTag+1:thirdTag],(int(book[firstTag+1:secondTag])-(element[2]+element[3]))])
 def addFine(days, name):
     True
+def addAllFine():
+    True
 
 def ratioBook():
     for book in bookList:
